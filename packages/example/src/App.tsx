7681cf2a94ddfb4efe9008cf949c826c99efb903
afeiship <1290657123@qq.com>
2024-01-22 22:17:44 +0800

    feat: add interactive

--- packages/example/src/App.tsx
+++ packages/example/src/App.tsx
@@ -1,18 +1,18 @@
-import TheComponent from '@jswork/react-smartphoto';
-import '@jswork/react-smartphoto/src/style.scss';
+import TheComponent from '../../lib/src';
+import '../../lib/src/style.scss';
 import './App.css';
 
-
 function App() {
+  const items = Array.from({ length: 8 }).map((_, i) => ({
+    url: `https://randomuser.me/api/portraits/lego/${i}.jpg`,
+    caption: `Image ${i}`,
+    id: i,
+  }));
+
   return (
     <>
       <h1>react-smartphoto</h1>
-      <TheComponent
-        onClick={() => {
-          console.log('click me');
-        }}>
-        Click me
-      </TheComponent>
+      <TheComponent items={items}>Click me</TheComponent>
     </>
   );
 }
--- packages/lib/src/index.tsx
+++ packages/lib/src/index.tsx
@@ -1,10 +1,59 @@
 import React from 'react';
+import ReactList from '@jswork/react-list';
+import SmartPhoto from 'smartphoto';
+import clsx from 'classnames';
 
-export interface ButtonProps {
-  children: React.ReactNode;
-  onClick: () => void;
+const CLASS_NAME = 'react-smartphoto';
+
+interface SmartphotoOptions {
+  arrows?: boolean;
+  nav?: boolean;
+  useOrientationApi?: boolean;
+  resizeStyle?: 'fill' | 'fit';
+  animationSpeed?: number;
+  forceInterval?: number;
 }
 
-export default function Button({ children, onClick }: ButtonProps) {
-  return <button onClick={onClick}>{children}</button>;
+interface TemplateProps {
+  (inEvent: { item: any; index: number; items: any[] }): React.ReactNode;
+}
+
+export interface ReactSmartphotoProps {
+  className?: string;
+  items: any[];
+  template?: TemplateProps;
+  as?: React.ElementType;
+  asProps?: any;
+  options?: SmartphotoOptions;
+}
+
+export default class ReactSmartphoto extends React.Component<ReactSmartphotoProps> {
+  static displayName = CLASS_NAME;
+  private smartphoto: SmartPhoto;
+
+  componentDidMount() {
+    const { options } = this.props;
+    this.smartphoto = new SmartPhoto(`.${CLASS_NAME}__item`, options);
+  }
+
+  template = ({ item, index }) => {
+    return (
+      <a key={index} href={item.url} data-caption={item.caption} className={`${CLASS_NAME}__item`}>
+        <img src={item.url} alt="" />
+      </a>
+    );
+  };
+
+  render() {
+    const { items, template, options, className, ...props } = this.props;
+    return (
+      <ReactList
+        className={clsx(CLASS_NAME, className)}
+        as="div"
+        items={items}
+        template={this.template}
+        {...props}
+      />
+    );
+  }
 }
